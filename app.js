83d12304bacf324523dfb9b905a970ff2a319037
pierre.biojoux@outlook.com <pierre.biojoux@outlook.com>
2018-03-20 01:53:03 +0000

    Update the package.json and remove less-middleware

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var lessMiddleware = require('less-middleware');
 
 // Require the routes
 var index = require('./routes/index');
 var instance = require('./routes/instance');
 var welcome = require('./routes/welcome');
+var login = require('./routes/login');
 
 // Creating the app with express
 var app = express();
@@ -30,12 +30,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/index', index);
 app.use('/instance', instance);
 app.use('/', welcome);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
