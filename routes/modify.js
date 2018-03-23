5cd2ec99a329bee09ce23bcad7ad7817ece164a6
pierre.biojoux@outlook.com <pierre.biojoux@outlook.com>
2018-03-23 16:15:59 +0000

    Click on the button modify open a page  - Change the button from welcome to href in order to open the page with the name in parameter of url  - Create the route modify and jade modify

---
+++ routes/modify.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var GenoverseInstance = require('../models/GenoverseInstance.js');
+
+/* GET genome page. */
+router.get('/', function (req, res, next) {
+    GenoverseInstance.find({name: req.query.name}, function (err, instance) {
+        if (err) {
+            res.send(err);
+            throw err;
+        } else if (instance.length) {
+            console.log('Object loaded');
+            res.render('modify', {object: instance[0]});
+        }
+    });
+
+});
+module.exports = router;
