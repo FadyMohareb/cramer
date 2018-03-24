f9a097005fe3b3d7ba56fb7e5974b2a2140ae68f
pierre.biojoux@outlook.com <pierre.biojoux@outlook.com>
2018-03-24 13:29:34 +0000

    Automatic display of plugins and list species  - list species from ensembl download and save in a json  - plugins in a json  - update the style with col-md-*  - update package json with async

--- routes/instance.js
+++ routes/instance.js
@@ -1,18 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
 var dir = process.cwd();
 var fs = require('fs');
+var GenoverseInstance = require('../models/GenoverseInstance.js');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.render('instance');
-});
 
+    async.parallel({
+        species: function (callback) {
+            fs.readFile(dir + '/public/javascript/genomes/list-species.json', 'utf8', function (err, data) {
+                if (err) {
+                    throw err;
+                    callback(err);
+                }
+                callback(null, JSON.parse(data));
+            });
+        },
+        plugins: function (callback) {
+            fs.readFile(dir + '/public/javascript/plugins/list-plugins.json', 'utf8', function (err, data) {
+                if (err) {
+                    throw err;
+                    callback(err);
+                }
+                callback(null, JSON.parse(data));
+            });
+        }
+    },
+            function (err, results) {
+                // results is now equals to: {one: 1, two: 2}
+                res.render('instance', {listSpecies: results.species, listPlugins: results.plugins});
+            });
+
+
+});
 router.post('/', function (req, res, next) {
 
     console.log('body: ' + JSON.stringify(req.body));
     var obj = req.body;
-
     setUpParameters(obj, function (valideConfig, valideGenome) {
         if (valideConfig && valideGenome) {
             res.end('done');
@@ -23,85 +49,43 @@ router.post('/', function (req, res, next) {
         }
     });
 });
-
 function setUpParameters(obj, callback) {
     var valideGenome = fs.existsSync(dir + '/public/javascript/genomes/' + 'human' + '.js');
-
     if (!valideGenome) {
         console.log('Genome Does Not Exist Yet');
-        createGenome(obj);
+        createGenome(obj.genome);
         valideGenome = fs.existsSync(dir + '/public/javascript/genomes/' + 'human' + '.js');
     } else {
         console.log('Genome File Already Exists');
     }
 
-    var valideConfig = writeConfig(obj);
+    valideConfig = addDatabase(obj);
     callback(valideConfig, valideGenome);
 }
 
-
-function writeConfig(obj) {
-    console.log('Write the config file');
-
-    try {
-        var parameters = fs.readFileSync(dir + "/public/javascript/config/config-genoverse-template.js", "utf8")
-                .replace(/__CHROM__/, obj.chromosome)
-                .replace(/__START__/, obj.start)
-                .replace(/__END__/, obj.end)
-                .replace(/__PLUGINS__/, JSON.stringify(obj.plugins));
-
-        fs.writeFile(dir + "/public/javascript/config/config-genoverse.js", parameters, "utf8", function () {
-            console.log("Config File Done");
-        });
+function addDatabase(obj) {
+    var instance = new GenoverseInstance({
+        "name": obj.name,
+        "description": obj.description,
+        "genome": obj.genome,
+        "chr": obj.chr,
+        "start": obj.start,
+        "end": obj.end,
+        "plugins": obj.plugins,
+        "tracks": "",
+    });
+    instance.save(function (err) {
+        if (err) {
+            throw err;
+            return false;
+        }
+        console.log('Commentaire ajouté avec succès !');
         return true;
-    } catch (e) {
-        console.log("Error writing the config file \n" + e.message);
-        return false;
-    }
-
-}
-
-function updateSpecies() {
-    console.log('Write all the species in a file');
-
-// Create a species file for the Ensembl REST API
-    require('http').get({
-        hostname: 'rest.ensembl.org',
-        path: '/info/species?',
-        headers: {'Content-Type': 'application/json'}
-    }, function (response) {
-        var str = '';
-
-        response.on('data', function (chunk) {
-            str += chunk;
-        });
-
-        response.on('end', function () {
-            try {
-                require('fs').writeFile(dir + '/public/javascript/genomes/list-species.js', 'Species = ' + JSON.stringify(JSON.parse(str).species.map(function (d) {
-                    return [
-                        d.name, {
-                            display_name: d.display_name
-                        }
-                    ];
-                }).reduce(function (hash, d) {
-                    hash[d[0]] = d[1];
-                    return hash;
-                }, {}), null, 2), function () {
-                    console.log('Species File Done');
-                });
-            } catch (e) {
-                console.log(e.message);
-            }
-        });
-    }).on('error', function (e) {
-        console.log(`Got error: ${e.message}`);
-    }).end();
+    });
 }
 
 function createGenome(obj) {
     console.log('Write the genome file');
-
     // Create a genome file from the Ensembl REST API
     require('http').get({
         hostname: 'rest.ensembl.org',
@@ -109,11 +93,9 @@ function createGenome(obj) {
         headers: {'Content-Type': 'application/json'}
     }, function (response) {
         var str = '';
-
         response.on('data', function (chunk) {
             str += chunk;
         });
-
         response.on('end', function () {
             try {
                 require('fs').writeFile(dir + '/public/javascript/genomes/human.js', 'Genoverse.Genomes.' + 'human' + ' = ' + JSON.stringify(JSON.parse(str).top_level_region.filter(function (d) {
@@ -140,7 +122,42 @@ function createGenome(obj) {
     }).on('error', function (e) {
         console.log(`Got error: ${e.message}`);
     }).end();
+}
 
+function updateSpecies() {
+    console.log('Write all the species in a file');
+// Create a species file for the Ensembl REST API
+    require('http').get({
+        hostname: 'rest.ensembl.org',
+        path: '/info/species?',
+        headers: {'Content-Type': 'application/json'}
+    }, function (response) {
+        var str = '';
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+        response.on('end', function () {
+            try {
+                require('fs').writeFile(dir + '/public/javascript/genomes/list-species.json', JSON.stringify(JSON.parse(str).species.map(function (d) {
+                    return [
+                        d.name, {
+                            display_name: d.display_name,
+                            id: d.name
+                        }
+                    ];
+                }).reduce(function (hash, d) {
+                    hash[d[0]] = d[1];
+                    return hash;
+                }, {}), null, 2), function () {
+                    console.log('Species File Done');
+                });
+            } catch (e) {
+                console.log(e.message);
+            }
+        });
+    }).on('error', function (e) {
+        console.log(`Got error: ${e.message}`);
+    }).end();
 }
 
 module.exports = router;
